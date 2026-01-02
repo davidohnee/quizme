6e56bc4a4a5cf81cf77d67cea7aa0780f12700f7
dxstiny <david.diashorta@gmail.com>
2026-01-02 13:06:42 +0100

    update github actions & remove dist from repo

--- dist/assets/Redirect-Db_c1SUf.js
+++
@@ -1,1 +0,0 @@
-import{d as t,e as r,i as u}from"./index-Bdt2vfFF.js";const c=t({__name:"Redirect",setup(n){const s=u(),e=r();if(e.courses.length===0)s.push("/");else{const o=e.courses[Math.floor(Math.random()*e.courses.length)].id;s.push(`/quiz/${o}`)}return()=>{}}});export{c as default};
